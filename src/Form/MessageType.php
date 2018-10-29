372d0c288b299359c70c8aff14530bfc073a5ca2
Dmitry Litvin <dlitvin@ooo-alfatel.ru>
2018-10-29 10:11:47 +0300

    835682456

---
+++ src/Form/MessageType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Message;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MessageType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text')
+            ->add('isRead')
+            ->add('root')
+            ->add('level')
+            ->add('left')
+            ->add('right')
+            ->add('sender')
+            ->add('recipient')
+            ->add('parent')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Message::class,
+        ]);
+    }
+}
