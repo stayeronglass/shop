7abbe9df6f0e9b62ee632430e5b5ae1ee0ff9ffa
stayeronglass <stayeronglass@gmail.com>
2018-11-26 01:43:54 +0300

    5556709

--- src/Form/DeliveryType.php
+++ src/Form/DeliveryType.php
@@ -3,6 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Product;
+use App\Repository\ProductRepository;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -14,12 +17,14 @@ class DeliveryType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('delivery', ChoiceType::class, [
-                'choices' => [
-                        '111',
-                        '222',
-                        '333',
-                ],
+            ->add('delivery', EntityType::class, [
+                'choice_label' => 'title',
+                'class' => Product::class,
+                'query_builder' => function(ProductRepository $er) {
+                    return $er->createQueryBuilder('p')
+                        ->andWhere('p.id in(297, 298) ')
+                        ->orderBy('p.id', 'ASC');
+                },
                 'expanded' => true,
                 'multiple' => false,
             ]);
