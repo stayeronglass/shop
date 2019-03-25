3ef575321792afe68229e79d44e36403e20d7c91
Dmitry Litvin <dlitvin@ooo-alfatel.ru>
2019-03-25 13:19:06 +0300

    83568245152

---
+++ src/Admin/ImageAdmin.php
@@ -0,0 +1,59 @@
+<?php
+namespace App\Admin;
+
+use App\Entity\Image;
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+
+class ImageAdmin extends AbstractAdmin
+{
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('id')
+        ;
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+
+        $listMapper
+            ->add('id')
+            ->add('name')
+        ;
+    }
+
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('file', FileType::class, [
+                'required' => false
+            ])
+        ;
+    }
+
+
+    public function prePersist($image)
+    {
+        $this->manageFileUpload($image);
+    }
+
+
+    public function preUpdate($image)
+    {
+        $this->manageFileUpload($image);
+    }
+
+
+    private function manageFileUpload(Image $image)
+    {
+        if ($image->getFile()) {
+            $image->refreshUpdated();
+        }
+    }
+
+}
--- src/Entity/Image.php
+++ src/Entity/Image.php
@@ -12,6 +12,13 @@ use Gedmo\Timestampable\Traits\TimestampableEntity;
  */
 class Image
 {
+    const SERVER_PATH_TO_IMAGE_FOLDER = '/server/path/to/images';
+    /**
+     * Unmapped property to handle file uploads
+     */
+    private $file;
+
+
     use TimestampableEntity;
     /**
      * @ORM\Id()
@@ -115,5 +122,65 @@ class Image
     }
 
 
-       
+    /**
+     * @param UploadedFile $file
+     */
+    public function setFile(UploadedFile $file = null)
+    {
+        $this->file = $file;
+    }
+
+    /**
+     * @return UploadedFile
+     */
+    public function getFile()
+    {
+        return $this->file;
+    }
+
+    /**
+     * Lifecycle callback to upload the file to the server.
+     */
+    public function lifecycleFileUpload()
+    {
+        $this->upload();
+    }
+
+    /**
+     * Updates the hash value to force the preUpdate and postUpdate events to fire.
+     */
+    public function refreshUpdated()
+    {
+        $this->setUpdatedAt(new \DateTime());
+    }
+
+
+    /**
+     * Manages the copying of the file to the relevant place on the server
+     */
+    public function upload()
+    {
+        // the file property can be empty if the field is not required
+        if (null === $this->getFile()) {
+            return;
+        }
+
+        // we use the original file name here but you should
+        // sanitize it at least to avoid any security issues
+
+        // move takes the target directory and target filename as params
+        $this->getFile()->move(
+            self::SERVER_PATH_TO_IMAGE_FOLDER,
+            $this->getFile()->getClientOriginalName()
+        );
+
+        // set the path property to the filename where you've saved the file
+        $this->filename = $this->getFile()->getClientOriginalName();
+
+        // clean up the file property as you won't need it anymore
+        $this->setFile(null);
+    }
+
+
+
 }
