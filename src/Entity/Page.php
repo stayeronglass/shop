108b895faacd2694163cca3fb034d59ccb6d5d65
stayeronglass <153424009b>
2019-07-11 16:28:11 +0300

    275684068902

--- src/Entity/Page.php
+++ src/Entity/Page.php
@@ -59,6 +59,18 @@ class Page
         return $this;
     }
 
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
     public function getH1(): ?string
     {
         return $this->h1;
@@ -82,4 +94,5 @@ class Page
 
         return $this;
     }
+
 }
